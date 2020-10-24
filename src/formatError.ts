5e53e6b0f4ec41b9e463a978eb92c22f0e704a4f
Niklas Korz <niklas@niklaskorz.de>
2020-10-24 17:02:57 +0200

    Apply code style suggestion

--- src/formatError.ts
+++ src/formatError.ts
@@ -1,7 +1,7 @@
 const trimLeft = "GraphQL error: ";
-export default (message: string): string => {
+export default function formatError(message: string): string {
   if (message.startsWith(trimLeft)) {
     return message.slice(trimLeft.length);
   }
   return message;
-};
+}
